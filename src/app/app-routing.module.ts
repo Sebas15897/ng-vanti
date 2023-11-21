8f2077f15da8820cc1a36c5b7993062b3bff2c6c
Sebas15897 <sebastiancontreras15897@gmail.com>
2023-11-20 21:28:45 -0500

    feat: In this commit the user list component is created, the service and an interface are added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
 
-
+const routes: Routes = [
+  {
+    path: 'users',
+    component: UserListComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/users',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/core/interfaces/user.interface.ts
@@ -0,0 +1,5 @@
+export interface IUser {
+  id: number;
+  nombre: string;
+  correo_electronico: string;
+}
